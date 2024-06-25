7fd76ed41e0774d1758f1195727e567cd864336a
Georg Graf (SPG) <grafg@spengergasse.at>
2024-06-25 14:39:57 +0200

    stunde mit htmx und express

---
+++ 2024-06-25__express/index.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express');
+
+// Create the express app
+const app = express();
+
+// Routes and middleware
+// app.use(/* ... */)
+// app.get(/* ... */)
+app.get("/datum", (req, res) => {
+  res.send(new Date().toLocaleString());
+});
+app.use(express.static('static'));
+// Error handlers
+app.use(function fourOhFourHandler(req, res) {
+  res.status(404).send();
+});
+app.use(function fiveHundredHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).send();
+});
+
+// Start server
+app.listen(1234, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('Started at http://localhost:1234');
+});
